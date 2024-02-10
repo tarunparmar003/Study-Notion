0b11238f9c4f18682202be06ff8c048c3aae2192
Ujjwal Kashyap <ujjwalkashyap2511@gmail.com>
2024-02-11 05:12:49 +0530

    check-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,7 +110,7 @@ const Home =()=>
               active: false,
             }}
             codeColor={"text-white"}
-            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default  mu ujgh Home;`}
+            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
             backgroundGradient={<div className="codeblock2 absolute"></div>}
           />
         </div>
